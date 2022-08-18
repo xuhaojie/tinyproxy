e72961841cd828ca9f151ec9d4eee66b1eb92986
xuhaojie <xuhaojie@hotmail.com>
2022-08-18 19:00:34 +0800

    http&https都可以工作了

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,11 @@ use std::io;
 use async_std::task;
 use url::{Host, Origin, Url};
 
-
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
 	let server = TcpListener::bind("127.0.0.1:8088").await.unwrap();
 	while let Ok((client_stream, client_addr)) = server.accept().await {
-		println!("accept client: {}", client_addr);
+//		println!("accept client: {}", client_addr);
 		// 每接入一个客户端的连接请求，都分配一个子任务，
 		// 如果客户端的并发数量不大，为每个客户端都分配一个thread，
 		// 然后在thread中创建tokio runtime，处理起来会更方便
@@ -23,8 +22,13 @@ async fn main() -> std::io::Result<()> {
 	Ok(())
 }
 
+enum Method {
+	CONNECT,
+	OTHER,
+}
+
 async fn process_client(mut client_stream: TcpStream) -> io::Result<()> {
-	let addr = &client_stream.local_addr()?;
+	let addr = &client_stream.peer_addr()?;
 	println!("client addr:{:?}",addr);
 	// Read the CONNECT request
 	let mut buf = [0; 4096];
@@ -32,27 +36,22 @@ async fn process_client(mut client_stream: TcpStream) -> io::Result<()> {
 
 	let mut req = String::from("");
 	match std::str::from_utf8(&buf[..nbytes]){
-	Ok(s) => {req = String::from(s)},
-	Err(e) => {
-		println!("error: {:?}", e);
-		
+		Ok(s) => {req = String::from(s)},
+		Err(e) => {
+			println!("error: {:?}", e);
+			
+		}
 	}
-	}
-
-
-	println!("Received request {}", req);
 
 	let mut address : String;
 	let v: Vec<&str> = req.split(' ').collect();
-	if v[0] == "CONNECT" {
-		println!(":CONNECT {}", v[1]);
+	let method = if v[0] == "CONNECT" {
 		address = String::from(v[1]);
-
+		println!(":CONNECT address: {}", address);
+		Method::CONNECT
 	} else {
-		println!(":{} {}", v[0], v[1]);
 		address = String::from(v[1]);
-		println!("{}", address);
-		
+		// println!("{}", address);
 		if address.starts_with("http://"){
  
 			match Url::parse(v[1]) {
@@ -74,34 +73,30 @@ async fn process_client(mut client_stream: TcpStream) -> io::Result<()> {
 				}
 				Err(err) => println!("{}", err),
 			}
-
-/*
-			let tmp_str = String::from(address).replace("http://", "");
-			let ac = tmp_str.chars().count();
-	
-			let mut new_tmp_str = tmp_str[..ac-1].to_owned(); 
-			new_tmp_str.push_str(":80");
-			address = new_tmp_str;
-*/			
 			println!("address: {}", address);
+		} else{
+			println!("???? address: {}", address);
 		}
+		Method::OTHER
+	};
 
 
-	}
-
-   client_stream.write_all(b"HTTP/1.1 200 Connection established\r\n\r\n").await?;
 
 	// Connect to a peer
-	
-	
-	//let mut server_stream = TcpStream::connect(address,).await?;
 	let mut server_stream = TcpStream::connect(address,).await?;
 
 	let (lr, lw) = &mut (&client_stream, &client_stream);
 	let (tr, tw) = &mut (&server_stream, &server_stream);
 
-//	tw.write(action.as_bytes());
-
+	match method {
+		Method::CONNECT => {
+			lw.write_all(b"HTTP/1.1 200 Connection established\r\n\r\n").await?;
+		} ,
+		_ => {
+			tw.write_all(req.as_bytes()).await?;
+		},
+	}
+	
     let copy_a = async_std::io::copy(lr, tw);
     let copy_b = async_std::io::copy(tr, lw);
 	
