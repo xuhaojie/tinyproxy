aabf0bbd94bd216768a44f9137925039c1ae8fcb
xuhaojie <xuhaojie@hotmail.com>
2022-08-18 22:41:14 +0800

    修正死循环问题

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use async_std::{
 };
 use futures::future::FutureExt;
 use std::io;
+use std::time::Instant;
 use url::Url;
 use log::*;
 
@@ -16,9 +17,14 @@ async fn main() -> std::io::Result<()> {
 	let server = TcpListener::bind(server_address).await.unwrap();
 	info!("listening on {}", server_address);
 	while let Ok((client_stream, client_addr)) = server.accept().await {
-		info!("accept client: {}", client_addr);
+		debug!("accept client: {}", client_addr);
 		task::spawn(async {
-			process_client(client_stream).await;
+			//let start = Instant::now();
+			match process_client(client_stream).await {
+				Ok(()) => (),
+				Err(e) => error!("error: {}", e),
+			}
+			//debug!("finished in {} ms.", start.elapsed().as_millis())
 		});
 	}
 	Ok(())
@@ -30,38 +36,69 @@ enum Method {
 }
 
 async fn process_client(mut client_stream: TcpStream) -> io::Result<()> {
-	let addr = &client_stream.peer_addr()?;
-	info!("client addr:{:?}",addr);
+	let client_addr = &client_stream.peer_addr()?;
 	// Read the CONNECT request
-	let mut buf = [0; 256];
-	let bytes = client_stream.read(&mut buf).await?;
+	let mut buf = [0; 4096];
+	let mut count = 0;
+	loop {
+		let bytes = client_stream.read(&mut buf[count..]).await?;
+		if bytes > 0 {
+			count += bytes;
+			if count > 8 {
+				break;
+			}
+		}
+	};
 
-	let mut request;
-	match std::str::from_utf8(&buf[..bytes]){
-		Ok(s) => {request = String::from(s)},
+	let request;
+	let mut index = 0;
+	for i in buf.iter() {
+		if *i == '\n' as u8 {
+			break;
+		}
+		if index <= count {
+			index += 1;
+		} else{
+			break
+		}
+	}
+
+	if index < 3 {
+		return Err(io::Error::new(io::ErrorKind::Other, "request to short"));
+	}
+
+	debug!("index: {:?}", index);
+
+	match std::str::from_utf8(&buf[..index]){
+	Ok(s) => {
+			request = String::from(s);
+		},
 		Err(e) => {
 			error!("error: {:?}", e);
 			return Err(io::Error::new(io::ErrorKind::Other, e.to_string()));
 		}
 	}
-
+	
 	let mut address : String;
 	let v: Vec<&str> = request.split(' ').collect();
 	if v.len() < 2 {
-		io::Error::new(io::ErrorKind::Other, "failed parse request");
+		error!("bad request: {}", request);
+		return Err(io::Error::new(io::ErrorKind::Other, "failed parse request"));
 	}
+
+	info!("{} -> {}", client_addr.to_string(), request);
+
+	address = String::from(v[1]);
 	let method = if v[0] == "CONNECT" {
-		address = String::from(v[1]);
-		info!(":CONNECT address: {}", address);
+		debug!("CONNECT address: {}", address);
 		Method::CONNECT
 	} else {
-		address = String::from(v[1]);
-		if address.starts_with("http://"){
- 
-			match Url::parse(v[1]) {
+		debug!("{} address: {}",v[0], address);
+		if address.starts_with("http://") {
+ 			match Url::parse(v[1]) {
 				Ok(url) => {
 					let addr = url.host().unwrap();
-					let mut port :u16 = match url.port(){Some(p) => p,	None => 80,};
+					let port :u16 = match url.port(){Some(p) => p,	None => 80,};
 					address = format!("{}:{}",addr.to_string(), port)
 				}
 				Err(err) => println!("{}", err),
@@ -74,26 +111,26 @@ async fn process_client(mut client_stream: TcpStream) -> io::Result<()> {
 	};
 
 	// Connect to a peer
-	let mut server_stream = TcpStream::connect(address,).await?;
+	let server_stream = TcpStream::connect(address,).await?;
 
-	let (lr, lw) = &mut (&client_stream, &client_stream);
-	let (tr, tw) = &mut (&server_stream, &server_stream);
+	let (local_reader, local_writer) = &mut (&client_stream, &client_stream);
+	let (server_reader, server_writer) = &mut (&server_stream, &server_stream);
 
 	match method {
 		Method::CONNECT => {
-			lw.write_all(b"HTTP/1.1 200 Connection established\r\n\r\n").await?;
+			local_writer.write_all(b"HTTP/1.1 200 Connection established\r\n\r\n").await?;
 		} ,
 		_ => {
-			tw.write_all(&buf).await?;
+			server_writer.write_all(&buf).await?;
 		},
 	}
 	
-    let copy_a = async_std::io::copy(lr, tw);
-    let copy_b = async_std::io::copy(tr, lw);
+    let copy_task_a = async_std::io::copy(local_reader, server_writer);
+    let copy_task_b = async_std::io::copy(server_reader, local_writer);
 	
-    let r = futures::select! {
-        r1 = copy_a.fuse() => r1,
-        r2 = copy_b.fuse() => r2
+    let _ = futures::select! {
+        r1 = copy_task_a.fuse() => r1,
+        r2 = copy_task_b.fuse() => r2
     };
 
     Ok(())
