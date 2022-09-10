d49cebff6859265052e91df6fd59810b8d57f420
xuhaojie <xuhaojie@hotmail.com>
2022-09-10 18:27:35 +0800

    简化代码

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,36 @@ use async_std::{ net::{SocketAddr, TcpListener, TcpStream},	prelude::*,	task,};
 use futures::future::FutureExt;
 use log::*;
 use url::Url;
-use std::result::Result::Ok;
-use anyhow::*;
+
+use anyhow::{*,Result};
 
 #[async_std::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
 	env_logger::init();
 	let server_address = "0.0.0.0:8088";
 	let server = TcpListener::bind(server_address).await.unwrap();
 	info!("listening on {}", server_address);
-	while let Ok((client_stream, client_addr)) = server.accept().await {
+	while let Result::Ok((client_stream, client_addr)) = server.accept().await {
 		debug!("accept client: {}", client_addr);
 		task::spawn(async move {
 			match process_client(client_stream, client_addr).await {
-				Ok(()) => (),
+				anyhow::Result::Ok(()) => (),
 				Err(e) => error!("error: {}", e),
 			}
 		});
 	}
 	Ok(())
 }
-
-async fn process_client(mut client_stream: TcpStream, client_addr: SocketAddr) -> anyhow::Result<()> {
+  
+async fn process_client(mut client_stream: TcpStream, client_addr: SocketAddr) -> Result<()> {
 	let mut buf = [0; 1024];
-	
 	let count = client_stream.read(&mut buf).await?;
 	if count == 0 {
 		return Ok(());
 	}
 
 	let request = String::from_utf8_lossy(&buf);
-	let line = request.lines().next().unwrap();
+	let line = request.lines().next().unwrap_or("");
 	let fields: Vec<&str> = line.split_whitespace().collect();
 	if fields.len() < 2 {
 		return Err(anyhow!("bad request"));
@@ -44,17 +43,15 @@ async fn process_client(mut client_stream: TcpStream, client_addr: SocketAddr) -
 	let url = Url::parse(fields[1])?;
 	let (https, address) = if method == "CONNECT" {
 		(true, String::from(fields[1]))
-	} else {
-		(
-			false,
-				if let Some(addr) = url.host() {
-					let port: u16 = url.port().unwrap_or(80);
-					format!("{}:{}", addr.to_string(), port)
-				} else {
-					return Err(anyhow!( "bad host in url"));
-				}
-		)
-	};
+	} else {(
+		false,
+		if let Some(addr) = url.host() {
+			let port: u16 = url.port().unwrap_or(80);
+			format!("{}:{}", addr.to_string(), port)
+		} else {
+			return Err(anyhow!( "bad host in url"));
+		}
+	)};
 
 	debug!("{} address: {}", method, address);
 
